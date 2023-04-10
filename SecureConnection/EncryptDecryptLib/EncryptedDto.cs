06e2237ac92fffcdc0d69aa25ff02c13957d1d97
Mafyou <mdupleix@hotmail.com>
2023-04-10 03:03:28 +0200

    Add project files.

---
+++ SecureConnection/EncryptDecryptLib/EncryptedDto.cs
@@ -0,0 +1,10 @@
+﻿namespace EncryptDecryptLib;
+
+public class EncryptedDto
+{
+    public string EncryptedText { get; set; } = string.Empty;
+    public string IV { get; set; } = string.Empty;
+    public string Key { get; set; } = string.Empty;
+    public string DigitalSignature { get; set; } = string.Empty;
+    public string Sender { get; set; } = string.Empty;
+}
---
+++ SecureConnection/Program.cs
@@ -0,0 +1,35 @@
+﻿using CertificateManager;
+using EncryptDecryptLib;
+using Microsoft.Extensions.DependencyInjection;
+
+Console.WriteLine("Hello World!");
+var serviceProvider = new ServiceCollection()
+    .AddCertificateManager()
+    .BuildServiceProvider();
+
+var cc = serviceProvider.GetService<CreateCertificates>();
+
+if (cc == null)
+    throw new Exception("no service");
+
+var cert3072 = CreateRsaCertificates.CreateRsaCertificate(cc, 3072);
+
+var text = "I have a big dog. You've got a cat. We all love animals!";
+
+Console.WriteLine("-- Encrypt Decrypt asymmetric --");
+Console.WriteLine("");
+
+var asymmetricEncryptDecrypt = new AsymmetricEncryptDecrypt();
+
+var encryptedText = asymmetricEncryptDecrypt.Encrypt(text,
+    Utils.CreateRsaPublicKey(cert3072));
+
+Console.WriteLine("");
+Console.WriteLine("-- Encrypted Text --");
+Console.WriteLine(encryptedText);
+
+var decryptedText = asymmetricEncryptDecrypt.Decrypt(encryptedText,
+    Utils.CreateRsaPrivateKey(cert3072));
+
+Console.WriteLine("-- Decrypted Text --");
+Console.WriteLine(decryptedText);
